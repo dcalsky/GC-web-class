45f4a7d224c14f03ebce40326f40eeb4be3d06d0
dcalsky <dcalsky@163.com>
2016-10-10 20:55:29 +0800

    content/demo0: add jquery demo

--- projects/0/page4.js
+++ projects/0/page4.js
@@ -1,9 +1,27 @@
 var div2 = document.getElementById('div2');
 
-div2.addEventListener('mouseover', function(e) {
-    this.nextSibling.style.background = "#1967d2";
+div2.addEventListener('mouseover', function() {
+    this.nextSibling.style.background = "#1976d2";
 })
 
-div2.addEventListener('mouseout', function(e) {
+div2.addEventListener('mouseout', function() {
     this.nextSibling.style.background = "#d2d2d2";
 })
+
+
+
+
+
+
+
+// $ ???
+
+
+// var div2 = $('#div2');
+// div2.mouseover(function() {
+//     $(this).next().css('background', '#1976d2')
+// })
+
+// div2.mouseout(function() {
+//     $(this).next().css('background', '#d2d2d2')
+// })
